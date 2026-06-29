e55ba0c6d06164f13b1efef2e22cdaa2958e4b04
Flopster101 <nahuelgomez329@gmail.com>
2026-06-28 22:08:45 -0300

    Revert "s5e8825: Setup Neural Networks"
    This reverts commit 12036a085d93a107d670e14386c6dfec1491bb22.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- s5e8825-common-vendor.mk
+++ s5e8825-common-vendor.mk
@@ -18,7 +18,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/s5e8825-common/proprietary/vendor/etc/dqe/calib_data_rgbgain.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dqe/calib_data_rgbgain.xml \
     vendor/samsung/s5e8825-common/proprietary/vendor/etc/dqe/calib_data_skincolor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dqe/calib_data_skincolor.xml \
     vendor/samsung/s5e8825-common/proprietary/vendor/etc/dqe/calib_data_whitepoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dqe/calib_data_whitepoint.xml \
-    vendor/samsung/s5e8825-common/proprietary/vendor/etc/eden/gpu/eden_kernel_64.bin:$(TARGET_COPY_OUT_VENDOR)/etc/eden/gpu/eden_kernel_64.bin \
     vendor/samsung/s5e8825-common/proprietary/vendor/etc/gnss/ca.pem:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/ca.pem \
     vendor/samsung/s5e8825-common/proprietary/vendor/etc/gnss/gps.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.cfg \
     vendor/samsung/s5e8825-common/proprietary/vendor/etc/init/android.hardware.drm-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm-service.widevine.rc \
@@ -71,59 +70,22 @@ PRODUCT_PACKAGES += \
     lib_SoundAlive_SRC384_ver320 \
     lib_soundaliveresampler \
     libc++_shared \
-    libcommon \
-    libcommon_graphgen_export \
-    libcrane_compiler \
     libcrypto-tm \
-    libdove_compiler \
     libdpuhdr10p_8825 \
-    libeden_model \
-    libeden_nn \
-    libeden_osal \
-    libeden_profiler \
-    libeden_rt \
-    libeden_ud_cpu \
-    libeden_ud_dsp \
-    libeden_ud_gpu \
-    libeden_ud_npu \
-    libeden_xtool \
     libegis_fp_normal_sensor_test \
     libengmode_client \
-    libenn_dsp_dal \
-    libenn_dsp_fw_graph_parser \
-    libenn_dsp_kernels_cpu \
     libexynoscamera_cnr_plugin \
     libexynoscamera_plugin \
     libfloatingfeature \
     libgf_in_system_lib \
-    libgraphgen_ann_import \
-    libgraphgen_ann_import_s \
-    libgraphgen_ann_nnc \
-    libgraphgen_api \
-    libgraphgen_export \
-    libgraphgen_import \
-    libgraphgen_intermediate_process \
-    libgraphgen_misc \
-    libgraphgen_network \
     libhdr10p_meta_plugin_default \
     libhdr_plugin_exynos8825 \
     libhwjsqz \
-    libir \
-    libnpu_compiler \
-    libnpuc_api \
-    libnpuc_backend \
-    libnpuc_cmdq \
-    libnpuc_common \
-    libnpuc_controller \
-    libnpuc_frontend \
-    libnpuc_graph \
-    libnpuc_template \
     liboemcrypto \
     libprofileparamstorage \
     libqfp_sensortest \
     librecordalive \
     libril_sem \
-    libsait_npu_compiler \
     libsec-ril \
     libsecaudiocoreutils \
     libsecaudioinfo \
@@ -137,12 +99,10 @@ PRODUCT_PACKAGES += \
     libswcnr \
     libsynaFpSensorTestNwd \
     libteecl \
-    libtoco_graphgen_export \
     libuniapi \
     libuniplugin \
     libuuid_vendor \
     libvkmanager_vendor \
-    libvpl \
     libwrappergps \
     libwvaidl \
     sensors.grip \
